af5eec07bc65d3d238ef63a5940ec2f02dc7f1e1
krabaton <krabatua@gmail.com>
2018-09-12 02:11:52 +0300

    update

--- gulp/tasks/js.foundation.js
+++ gulp/tasks/js.foundation.js
@@ -2,28 +2,31 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function() {
-  $.gulp.task('js:foundation', function(cb) {
-    if ($.path.jsFoundation.length === 0) {
-      fs.mkdirSync($.config.root);
-      fs.mkdirSync(path.join($.config.root, 'assets'));
-      fs.mkdirSync(path.join($.config.root, 'assets/js'));
+module.exports = function () {
+  $
+    .gulp
+    .task('js:foundation', function (cb) {
+      if ($.path.jsFoundation.length === 0) {
+        fs.mkdirSync($.config.root);
+        fs.mkdirSync(path.join($.config.root, 'assets'));
+        fs.mkdirSync(path.join($.config.root, 'assets/js'));
 
-      let file = path.join($.config.root, 'assets/js');
-      file = !$.dev
-        ? path.join(file, 'foundation.min.js')
-        : path.join(file, 'foundation.js');
+        let file = path.join($.config.root, 'assets/js');
+        file = !$.dev
+          ? path.join(file, 'foundation.min.js')
+          : path.join(file, 'foundation.js');
 
-      fs.writeFile(file, 'console.log("Hello Node.js")', err => {
-        if (err) throw err;
-        cb();
-      });
-    } else {
-      return $.gulp
-        .src($.path.jsFoundation)
-        .pipe($.gp.concat('foundation.js'))
-        .pipe($.gp.if(!$.dev, $.gp.rename({ suffix: '.min' })))
-        .pipe($.gulp.dest($.config.root + '/assets/js'));
-    }
-  });
+        fs.writeFile(file, 'console.log("Hello Node.js")', err => {
+          if (err)
+            {throw err;}
+          cb();
+        });
+      } else {
+        return $
+          .gulp
+          .src($.path.jsFoundation)
+          .pipe($.gp.concat('foundation.js'))
+          .pipe($.gp.if(!$.dev, $.gp.rename({ suffix: '.min' }))).pipe($.gulp.dest($.config.root + '/assets/js'));
+      }
+    });
 };
--- gulp/tasks/js.process.js
+++ gulp/tasks/js.process.js
@@ -1,30 +1,27 @@
 'use strict';
 
-module.exports = function() {
-  $.gulp.task('js:process', function() {
-    var sourcePath = $.path.app.src,
-      bundles = $.path.app.bundles;
+module.exports = function () {
+  $
+    .gulp
+    .task('js:process', function () {
+      var sourcePath = $.path.app.src;
 
-    var bundled = bundles.map(function(bundle) {
-      return $.browserify({
-        entries: sourcePath + bundle,
-        debug: true
-      })
-      .transform($.babel, {presets: ['es2015']})
-      .bundle()
-      .on('error', $.gp.notify.onError({ title: 'JS' }))
-      .pipe($.source(bundle))
-      .pipe($.buffer())
-      .pipe($.gp.if($.dev, $.gp.sourcemaps.init({
-        loadMaps: true
-      })))
-      .pipe($.gp.if(!$.dev, $.gp.uglify()))
-      .pipe($.gp.if($.dev, $.gp.sourcemaps.write('./maps')))
-      .pipe($.gp.if(!$.dev, $.gp.rename({ suffix: '.min' })))
-      .pipe($.gulp.dest($.config.root + '/assets/js'));
+      var bundles = $.path.app.bundles;
+
+      var bundled = bundles.map(function (bundle) {
+        return $
+          .browserify({
+            entries: sourcePath + bundle,
+            debug: true
+          })
+          .transform($.babel, { presets: ['es2015'] })
+          .bundle()
+          .on('error', $.gp.notify.onError({ title: 'JS' }))
+          .pipe($.source(bundle))
+          .pipe($.buffer())
+          .pipe($.gp.if($.dev, $.gp.sourcemaps.init({ loadMaps: true }))).pipe($.gp.if(!$.dev, $.gp.uglify())).pipe($.gp.if($.dev, $.gp.sourcemaps.write('./maps'))).pipe($.gp.if(!$.dev, $.gp.rename({ suffix: '.min' }))).pipe($.gulp.dest($.config.root + '/assets/js'));
+      });
+
+      return $.merge(bundled);
     });
-
-    return $.merge(bundled);
-
-  });
 };
